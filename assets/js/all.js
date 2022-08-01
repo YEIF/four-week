363936c3f4cdc90fbf4a149a11e71b47996b24f3
IF <ast61006@gmail.com>
2022-08-01 15:53:15 +0800

    Update 2022-08-01T07:53:11.111Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,10 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  $('.ham-icon').click(function (e) {
+    e.preventDefault();
+    $('.dropdown').toggleClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
