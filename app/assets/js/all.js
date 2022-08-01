9dc3a9559cc36ba7b3b5324dcebf988bee28857e
IF <ast61006@gmail.com>
2022-08-01 15:51:21 +0800

    lV2

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,11 @@
-console.log('Hello!');
+
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+  $('.ham-icon').click(function(e) {
+    e.preventDefault();
+    $('.dropdown').toggleClass('active')
+  })
 });
+
+
